330a63001b9a47df68c8566f63a134ec4b10d400
Takudzwa L Chimanikire <tkchimanaz@gmail.com>
2022-08-24 00:51:48 +0200

    linked the add branch route with the controller

--- routes/country-routes.js
+++ routes/country-routes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 
 router.get('/get-all', countryController.getAll);
 router.post('/add-country', countryController.addCountry);
+router.post('/add-branch', countryController.addBranch);
 
 export default router;
