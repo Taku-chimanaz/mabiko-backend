61e74b4347eb3adade01ab5b1d291d2c4bdf5803
Takudzwa L Chimanikire <tkchimanaz@gmail.com>
2022-08-24 00:48:28 +0200

    added error message for missing resource

--- responses.js
+++ responses.js
@@ -1,2 +1,3 @@
 export const success = "request successful";
-export const internalErr = "Something went wrong"
+export const internalErr = "Something went wrong";
+export const notFoundErr = "resource not found"
