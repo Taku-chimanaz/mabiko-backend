8342d272394dfd878c53dea02c2dcbdc69e72e7b
Takudzwa L Chimanikire <tkchimanaz@gmail.com>
2022-08-24 01:35:31 +0200

    linked the remove branch route with its controller

--- routes/country-routes.js
+++ routes/country-routes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 router.get('/get-all', countryController.getAll);
 router.post('/add-country', countryController.addCountry);
-router.post('/add-branch', countryController.addBranch);
+router.put('/add-branch', countryController.addBranch);
+router.put('/remove-branch', countryController.removeBranch);
 
 export default router;
