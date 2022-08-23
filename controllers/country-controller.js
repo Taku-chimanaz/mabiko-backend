fcf3b8d746f97c153f764a2b361f1908538a757b
Takudzwa L Chimanikire <tkchimanaz@gmail.com>
2022-08-24 00:50:49 +0200

    added functionality for adding a new branch

--- controllers/country-controller.js
+++ controllers/country-controller.js
@@ -1,5 +1,5 @@
 import Country from "../Models/Country.js";
-import { success, internalErr } from "../responses.js";
+import { success, internalErr, notFoundErr} from "../responses.js";
 
 const getAll = (req,res)=> {
 
@@ -44,7 +44,39 @@ const addCountry = (req,res) => {
     })
 }
 
+const addBranch = async (req,res) => {
+
+    const {id, branches} = req.body;
+
+    const country = await Country.findById(id);
+    
+    if(country){
+        
+        country.branches = [...country.branches, ...branches];
+        const countrySaved = await country.save();
+
+        if(countrySaved){
+            res.json({
+                message: success,
+                country
+            })
+        }else {
+            res.status(500).json({
+                message: internalErr,
+                countrySaved
+            })
+        }
+
+    }else {
+
+        res.status(404).json({
+            message: notFoundErr,
+        })
+    }
+}
+
 export default {
     getAll,
-    addCountry
+    addCountry,
+    addBranch
 }
