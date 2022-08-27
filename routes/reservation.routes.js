a2ff42febff66719575a92fb54e8ac9272f49b99
Takudzwa L Chimanikire <tkchimanaz@gmail.com>
2022-08-27 19:30:44 +0200

    protected routes

--- routes/reservation.routes.js
+++ routes/reservation.routes.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import reservationController from '../controllers/reservation-controller.js';
+import  {verifyToken} from './../verify-token.js'
 const router = express.Router();
 
 
 router.get('/get-reservation/:id', reservationController.getReservation);
 router.post('/create-reservation', reservationController.createReservation);
 router.put('/update-reservation', reservationController.updateReservation);
-router.delete('/delete-reservation/:id', reservationController.deleteReservation);
+router.put('/cancel-reservation', reservationController.cancelReservation);
+router.delete('/delete-reservation/:id', verifyToken, reservationController.deleteReservation);
 
 export default router;
