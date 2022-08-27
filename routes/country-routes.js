2354486554ddbcc3d7f5d31817e4166f5e85fe5e
Takudzwa L Chimanikire <tkchimanaz@gmail.com>
2022-08-27 19:29:58 +0200

    protected routes

--- routes/country-routes.js
+++ routes/country-routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import countryController from './../controllers/country-controller.js'
+import { verifyToken } from '../verify-token.js';
 const router = express.Router();
 
 router.get('/get-all', countryController.getAll);
-router.post('/add-country', countryController.addCountry);
-router.put('/add-branch', countryController.addBranch);
-router.put('/remove-branch', countryController.removeBranch);
-router.delete('/delete-country/:id', countryController.deleteCountry);
+router.post('/add-country', verifyToken, countryController.addCountry);
+router.put('/add-branch', verifyToken, countryController.addBranch);
+router.put('/remove-branch', verifyToken, countryController.removeBranch);
+router.delete('/delete-country/:id', verifyToken, countryController.deleteCountry);
 
 export default router;
