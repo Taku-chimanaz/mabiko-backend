1fa1c9db8662c82512a018c1baf7cec84c72ad23
Takudzwa L Chimanikire <tkchimanaz@gmail.com>
2022-08-29 23:06:03 +0200

    added send-reservation-code route

--- routes/reservation.routes.js
+++ routes/reservation.routes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 router.get('/get-reservation/:id', reservationController.getReservation);
 router.post('/create-reservation', reservationController.createReservation);
 router.put('/update-reservation', reservationController.updateReservation);
+router.post('/send-reservation-code', reservationController.sendReservationCode)
 router.delete('/delete-reservation/:id', reservationController.deleteReservation);
 
 export default router;
