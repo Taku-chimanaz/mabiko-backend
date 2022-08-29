9329274a926a065f45e634c78b1bb4d5d6222fc4
Takudzwa L Chimanikire <tkchimanaz@gmail.com>
2022-08-29 23:04:44 +0200

    added feature to send verification email

--- controllers/reservation-controller.js
+++ controllers/reservation-controller.js
@@ -4,6 +4,8 @@ import {
     internalErrHandlerFunction, 
     notFoundHandlerFunction
 } from './../responses.js';
+import { businessEmail, password } from '../privateKey.js';
+import nodemailer from 'nodemailer';
 
 
 const getReservation = async(req,res) =>{
@@ -75,11 +77,61 @@ const deleteReservation  = (req,res)=>{
     })
 } 
 
+const sendReservationCode = (req,res)=>{
+
+    const {email} = req.body;
+
+    // generate verification code;
+
+    let randomCode = '';
+
+    for(let i = 0; i < 4; i++){
+
+        const randomNumber = Math.floor( Math.random() * 10);
+        randomCode += `${randomNumber}`;
+
+    }
+
+    
+    //  sending email
+
+    let mailTransporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: businessEmail,
+            pass: password
+        }
+    });
+
+    let mailDetails = {
+        from: businessEmail,
+        to: email,
+        subject: 'Mabiko Restaurant Reservation Verification',
+        html: `
+            <div style="display: grid; place-items: center; margin-left: auto; margin-right: auto; background-color: #ED4962;height: 150px; width: 250px">
+
+                <h3 style="color: white; text-align: center; margin-top: 1em">Verification Code</h3>
+                <p style="background-color: white; width: 50%; margin: 1em auto; padding: .5em; text-align: center">${randomCode}</p>
+
+            <div>
+        `
+    };
+
+    mailTransporter.sendMail(mailDetails, function(err, data) {
+        if(err) {
+            internalErrHandlerFunction(res, err)
+        } else {
+            successHandlerFunction(res, data)
+        }
+    });
+}
+
 
 
 export default {
     getReservation,
     createReservation,
     updateReservation,
-    deleteReservation
+    deleteReservation,
+    sendReservationCode
 }
